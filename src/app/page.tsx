cf681a47f2518c1bbdbd685ce3e46663885c672f
AliMohamed-SE <alimohamed.deveng@gmail.com>
2025-05-18 11:47:14 +0300

    SEO/remove opengraph from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,6 @@ export const metadata: Metadata = {
   title: "Home | Evander Creative Studio",
   description:
     "Welcome to Evander Creative Studio. We create visually compelling brand identities, UI/UX designs, and software solutions to help businesses thrive.",
-  openGraph: {
-    title: "Home | Evander Creative Studio",
-    description:
-      "Discover how Evander Creative Studio helps businesses establish a strong visual presence with branding, UI/UX design, and software solutions.",
-    url: "https://yourdomain.com",
-    images: ["/home-og.jpg"],
-  },
 };
 
 const Home = () => {
